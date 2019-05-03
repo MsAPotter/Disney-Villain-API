a571734524e408e4778abd4fb819153144404d50
Atasha Potter <Ms.APotter@gmail.com>
2019-05-03 11:37:12 -0400

    removed parser in connection file

--- db/connection.js
+++ db/connection.js
@@ -5,7 +5,7 @@ mongoose.Promise = Promise
 if (process.env.NODE_ENV == "production") {
 	mongoose.connect(process.env.DB_URL)
   } else {
-	mongoose.connect("mmongodb://localhost/disney-villain", { useNewUrlParser: true })
+	mongoose.connect("mmongodb://localhost/disney-villain")
 	.then((conn) => {
 		console.log(`connected to mongodb on ${conn.connections[0].name} db`)
 	.catch(err => {
